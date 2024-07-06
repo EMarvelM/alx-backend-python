d04b45740d7a6f26da56fc872337fba397f8b847
EMarvelM <marvelousegbe19@gmail.com>
2024-07-06 12:16:16 +0100

    add a funtion level documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,14 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating point numbers and returns the result.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
     return a + b
